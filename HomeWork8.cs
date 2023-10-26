649faaeed135218bb4e4db3bf7b60aad46eb0f29
Huynh0109 <142943075+Huynh0109@users.noreply.github.com>
2023-10-25 20:12:10 -0500

    Add files via upload

---
+++ HomeWork8.cs
@@ -0,0 +1,82 @@
+﻿namespace HomeWork8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Test Q1
+        int[] int_array = {11,23,31,42,53};
+        ArraySum(int_array);
+
+        // Input 2d array for Q2
+        int[,] array_2d = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Test Q2.1
+        PrintAllOddNumber(array_2d);
+
+        // Test Q2.2
+        Console.WriteLine($"\nSum of 2d array: {ElementSum(array_2d)}");
+
+        // Test Q2.3
+        int[,] Q2_3 = DoubleArray(array_2d);
+        Console.WriteLine("The new 2d array:");
+        foreach(int num in Q2_3){
+            Console.Write(num + " ");
+        }
+    }
+
+
+    // Q1: calculate the sum of elements in a given int_array (4 points)
+    public static void ArraySum(int[] int_array){
+        //put your answer below
+        int sum = int_array.Sum();
+        Console.WriteLine($"The Sum of int_array is: {sum}");
+
+    }
+
+
+    //Q2.1: given a 2d array, print all the odd elements (2 points)
+    public static void PrintAllOddNumber(int[ , ] array_2d){
+        //put your answer below
+       foreach(int odd in array_2d)
+       {
+        if(odd % 2 != 0)
+        {
+            Console.Write(odd + " ");
+        }
+       }
+    }
+
+    //Q2.2: given a 2d array, return the sum of all elements (2 points)
+    public static int ElementSum(int[ , ] array_2d){
+        //put your answer below
+       int sum1 = 0;
+       for(int i = 0; i < array_2d.GetLength(0); i++)
+       {
+        for(int j = 0; j < array_2d.GetLength(1); j++)
+        {
+            sum1 += array_2d[i, j];
+        }
+       }
+       return sum1;
+    }
+
+
+    // Q2.3: given a 2d array, double its element values and return it (2 points)
+    public static int[ , ] DoubleArray(int[ , ] array_2d){
+         //put your answer below
+       int[ , ] doubledArray = new int[array_2d.GetLength(0), array_2d.GetLength(1)];
+       for(int k = 0; k < array_2d.GetLength(0); k++)
+       {
+        for(int l = 0; l < array_2d.GetLength(1); l++)
+        {
+            doubledArray[k, l] = array_2d[k, l] * 2;
+        }
+       }
+       return doubledArray;
+    }  
+}
